ddb1872f7be4a541bf5ad1b6e8c70d9ed8ac401b
Khaled Elorbany <khaled.wagdy554@gmail.com>
2024-07-09 10:55:56 +0300

    implement router in index

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './modules/home/App';
+import { RouterProvider } from 'react-router-dom';
+import Router from './routes';
 import './index.css';
 
 const root = ReactDOM.createRoot(
@@ -8,6 +9,6 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={Router} />
   </React.StrictMode>
 );
