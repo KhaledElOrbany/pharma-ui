a207e48c92d54046147f7958b890c7209f9602b9
Khaled Elorbany <khaled.wagdy554@gmail.com>
2024-07-11 23:15:17 +0300

    added new state manuplator functions

--- src/modules/doctor/state/doctorState.ts
+++ src/modules/doctor/state/doctorState.ts
@@ -1,10 +1,11 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { doctorDetails } from '../types/doctor';
 
 const doctorSlice = createSlice({
   name: 'doctor',
   initialState: {
-    doctorDetails: {},
-    doctorsList: [],
+    doctorDetails: {} as doctorDetails,
+    doctorsList: [] as doctorDetails[],
   },
   reducers: {
     setDoctorDetails: (state, action) => {
@@ -13,8 +14,26 @@ const doctorSlice = createSlice({
     setDoctorsList: (state, action) => {
       state.doctorsList = action.payload;
     },
+    updateDoctorList: (state, action) => {
+      state.doctorsList = state.doctorsList.map((doctor: doctorDetails) => {
+        if (doctor.id === action.payload.id) {
+          return action.payload;
+        }
+        return doctor;
+      });
+    },
+    removeDoctorFromList: (state, action) => {
+      state.doctorsList = state.doctorsList.filter(
+        (doctor) => doctor.id !== action.payload.id
+      );
+    },
   },
 });
 
-export const { setDoctorDetails, setDoctorsList } = doctorSlice.actions;
+export const {
+  setDoctorDetails,
+  setDoctorsList,
+  updateDoctorList,
+  removeDoctorFromList,
+} = doctorSlice.actions;
 export default doctorSlice.reducer;
