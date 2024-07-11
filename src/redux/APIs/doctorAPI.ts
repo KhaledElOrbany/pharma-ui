bca23fab1ed100d3738adc0bbb70c3f2af770dde
Khaled Elorbany <khaled.wagdy554@gmail.com>
2024-07-11 21:39:59 +0300

    refactor: Update import paths for doctorSlice and doctorAPI

--- src/redux/APIs/doctorAPI.ts
+++ src/redux/APIs/doctorAPI.ts
@@ -1,15 +1,18 @@
 import { createApi } from '@reduxjs/toolkit/query/react';
 import { customBaseQueryWithoutAuth } from '../helpers/baseQuery';
-import { setDoctorDetails } from '../../modules/doctor/state';
+import {
+  setDoctorDetails,
+  setDoctorsList,
+} from '../../modules/doctor/state/doctorState';
 
-export const doctorAPI = createApi({
+const doctorAPI = createApi({
   reducerPath: 'doctorAPI',
   refetchOnReconnect: true,
   tagTypes: ['doctor'],
   baseQuery: customBaseQueryWithoutAuth,
   endpoints: (build) => ({
     fetchDoctor: build.query({
-      query: (id: any) => ({
+      query: (id: Number) => ({
         url: `/doctor/${id}`,
       }),
       transformResponse: (result: any) => result.data,
@@ -18,20 +21,27 @@ export const doctorAPI = createApi({
           .then((result) => {
             dispatch(setDoctorDetails({ payload: result.data }));
           })
-          .catch(({ error: err }) => {
-            // eslint-disable-next-line no-console
-            console.error(err);
-          });
+          .catch(() => {});
       },
       providesTags: ['doctor'],
     }),
     fetchDoctors: build.query({
       query: () => ({
         url: '/doctor/list',
+        method: 'GET',
       }),
+      transformResponse: (result: any) => result.data,
+      async onQueryStarted(_, { dispatch, queryFulfilled }) {
+        await queryFulfilled
+          .then((result) => {
+            dispatch(setDoctorsList({ payload: result.data }));
+          })
+          .catch(() => {});
+      },
       providesTags: ['doctor'],
     }),
   }),
 });
 
 export const { useFetchDoctorQuery, useFetchDoctorsQuery } = doctorAPI;
+export default doctorAPI;
