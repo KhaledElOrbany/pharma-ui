f1a12c16fc1f686d55ceeadf87358744656c7869
Khaled Elorbany <khaled.wagdy554@gmail.com>
2024-07-12 18:07:48 +0300

    use main route /

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,12 +1,17 @@
 import { createBrowserRouter, Outlet } from 'react-router-dom';
 
 import DoctorRoot from './modules/doctor/doctor';
+import Dashboard from './modules/dashboard/dashboard';
 
 const Router = createBrowserRouter([
   {
     path: '/',
     element: <Outlet />,
     children: [
+      {
+        path: '/',
+        element: <Dashboard />,
+      },
       {
         path: '/doctor',
         element: <DoctorRoot />,
