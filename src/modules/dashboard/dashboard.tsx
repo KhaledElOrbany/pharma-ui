e1ee5092d9f3f1ed5cf69b44a7ce8f0d6e1accfb
Khaled Elorbany <khaled.wagdy554@gmail.com>
2024-07-12 18:07:31 +0300

    rename index file

---
+++ src/modules/dashboard/dashboard.tsx
@@ -0,0 +1,13 @@
+import { Helmet } from 'react-helmet-async';
+
+export default function Dashboard() {
+  return (
+    <>
+      <Helmet>
+        <title>Dashbaord</title>
+      </Helmet>
+
+      <div>Dashboard to be :D</div>
+    </>
+  );
+}
