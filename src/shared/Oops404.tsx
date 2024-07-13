898f70412ba5022caeba625fe983f45dc98e56aa
Khaled Elorbany <khaled.wagdy554@gmail.com>
2024-07-13 17:50:02 +0300

    feat: Add 404 page and update Oops404 component

--- src/shared/Oops404.tsx
+++ src/shared/Oops404.tsx
@@ -1,3 +1,19 @@
 export default function Oops404() {
-  return <div>Oops! 404 Not Found</div>;
+  return (
+    <div
+      style={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
+      <img
+        src='/illustrations/404.svg'
+        alt='404'
+        style={{
+          height: 260,
+        }}
+      />
+    </div>
+  );
 }
