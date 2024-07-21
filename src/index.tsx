a30600877c6b05938c293024bf7113ce8c033662
Khaled Elorbany <khaled.wagdy554@gmail.com>
2024-07-21 19:27:51 +0300

    added i18n to index

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import { Provider } from 'react-redux';
 import { HelmetProvider } from 'react-helmet-async';
 import store from './redux/Store';
 import App from './modules/home/App';
+import './i18n';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
