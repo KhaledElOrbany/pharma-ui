c71b98cd4ccb1ce28fff5b2f3b39242899f5d11f
Khaled Elorbany <khaled.wagdy554@gmail.com>
2024-07-21 20:01:57 +0300

    Loader component

--- src/shared/components/loader/Loader.tsx
+++ src/shared/components/loader/Loader.tsx
@@ -1,3 +1,18 @@
+import { CircularProgress, styled } from '@mui/material';
+
+const StyledContainer = styled('div')(({ theme }) => ({
+  display: 'flex',
+  width: '100%',
+  height: '100%',
+  justifyContent: 'center',
+  padding: theme.spacing(12, 3),
+  backgroundColor: theme.palette.background.default,
+}));
+
 export default function Loader() {
-  return <div>Loading...</div>;
+  return (
+    <StyledContainer>
+      <CircularProgress size={40} color='primary' />
+    </StyledContainer>
+  );
 }
