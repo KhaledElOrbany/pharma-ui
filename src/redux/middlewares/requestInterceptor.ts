b5a2e2cce82a14de964b09f7afa6ec2cf416040c
Khaled Elorbany <khaled.wagdy554@gmail.com>
2024-07-29 22:05:10 +0300

    RequestInterceptor

--- src/redux/middlewares/requestInterceptor.ts
+++ src/redux/middlewares/requestInterceptor.ts
@@ -1,30 +1,10 @@
-// export const requestInterceptor =
-//   ({ dispatch }) =>
-//   (next) =>
-//   (action) => {
-//     if (
-//       action.payload?.status === 500 &&
-//       process.env.REACT_APP_ENV !== 'development'
-//     ) {
-//       window.location.href = '/500';
-//     } else if (
-//       action.payload?.status === 401 &&
-//       localStorage.getItem('token')
-//     ) {
-//       localStorage.removeItem('token');
-
-//       setTimeout(() => {
-//         if (typeof window !== 'undefined') window.location.reload();
-//       }, 1000);
-//     } else {
-//       next(action);
-//     }
-//   };
-
 const RequestInterceptor = () => (next: any) => (action: any) => {
   if (action.payload?.status === 500) {
-    // window.location.href = '/500';
-  } else if (action.payload?.status === 401 && localStorage.getItem('token')) {
+    window.location.href = '/500';
+  } else if (
+    (action.payload?.status === 401 || action.payload?.status === 403) &&
+    localStorage.getItem('token')
+  ) {
     localStorage.removeItem('token');
 
     setTimeout(() => {
