0e62ed2f10c5e5084cd8043d8246a4f0c5b9cf83
Khaled Elorbany <khaled.wagdy554@gmail.com>
2024-08-02 06:11:56 +0300

    Colors

--- src/theme/Colors.ts
+++ src/theme/Colors.ts
@@ -1,67 +1,96 @@
 const GREY = {
   0: '#FFFFFF',
-  100: '#F9FAFB',
-  200: '#F4F6F8',
-  300: '#DFE3E8',
-  400: '#C4CDD5',
-  500: '#919EAB',
-  600: '#637381',
-  700: '#454F5B',
-  800: '#212B36',
-  900: '#161C24',
+  50: '#fafafa',
+  100: '#f5f5f5',
+  200: '#eeeeee',
+  300: '#e0e0e0',
+  400: '#bdbdbd',
+  500: '#9e9e9e',
+  600: '#757575',
+  700: '#616161',
+  800: '#424242',
+  900: '#212121',
+  A100: '#d5d5d5',
+  A200: '#aaaaaa',
+  A400: '#303030',
+  A700: '#616161',
 };
 
-const PRIMARY = {
-  lighter: '#D1E9FC',
-  light: '#76B0F1',
-  main: '#2065D1',
-  dark: '#103996',
-  darker: '#061B64',
+const LIGHT_PRIMARY = {
+  main: '#1976d2',
+  light: '#42a5f5',
+  dark: '#1565c0',
 };
 
-const SECONDARY = {
-  lighter: '#D6E4FF',
-  light: '#84A9FF',
-  main: '#3366FF',
-  dark: '#1939B7',
-  darker: '#091A7A',
+const DARK_PRIMARY = {
+  main: '#90caf9',
+  light: '#e3f2fd',
+  dark: '#42a5f5',
 };
 
-const INFO = {
-  lighter: '#D0F2FF',
-  light: '#74CAFF',
-  main: '#1890FF',
-  dark: '#0C53B7',
-  darker: '#04297A',
+const LIGHT_SECONDARY = {
+  main: '#9c27b0',
+  light: '#ba68c8',
+  dark: '#7b1fa2',
 };
 
-const SUCCESS = {
-  lighter: '#AAF27F',
-  light: '#86f774',
-  main: '#54D62C',
-  dark: '#229A16',
-  darker: '#08660D',
+const DARK_SECONDARY = {
+  main: '#ce93d8',
+  light: '#f3e5f5',
+  dark: '#ab47bc',
 };
 
-const WARNING = {
-  lighter: '#FFF7CD',
-  light: '#FFE16A',
-  main: '#FFC107',
-  dark: '#B78103',
-  darker: '#7A4F01',
+const LIGHT_ERROR = {
+  main: '#d32f2f',
+  light: '#ef5350',
+  dark: '#c62828',
 };
 
-const ERROR = {
-  lighter: '#FFE7D9',
-  light: '#FFA48D',
-  main: '#FF4842',
-  dark: '#B72136',
-  darker: '#7A0C2E',
+const DARK_ERROR = {
+  main: '#f44336',
+  light: '#e57373',
+  dark: '#d32f2f',
+};
+
+const LIGHT_WARNING = {
+  main: '#ed6c02',
+  light: '#ff9800',
+  dark: '#e65100',
+};
+
+const DARK_WARNING = {
+  main: '#ffa726',
+  light: '#ffb74d',
+  dark: '#f57c00',
+};
+
+const LIGHT_INFO = {
+  main: '#0288d1',
+  light: '#03a9f4',
+  dark: '#01579b',
+};
+
+const DARK_INFO = {
+  main: '#29b6f6',
+  light: '#4fc3f7',
+  dark: '#0288d1',
+};
+
+const LIGHT_SUCCESS = {
+  main: '#2e7d32',
+  light: '#4caf50',
+  dark: '#1b5e20',
+};
+
+const DARK_SUCCESS = {
+  main: '#66bb6a',
+  light: '#81c784',
+  dark: '#388e3c',
 };
 
 const Colors = {
   lightBackgroundColor: '#ffffff',
-  darkBackgroundColor: '#ffffff',
+  darkBackgroundColor: '#0f1620', //'#0f1215', // '#141b2d',
   transparent: 'transparent',
   white: '#ffffff',
   black: '#000000',
@@ -69,12 +98,12 @@ const Colors = {
   teal: '#008080',
 
   grey: GREY,
-  primary: PRIMARY,
-  secondary: SECONDARY,
-  info: INFO,
-  success: SUCCESS,
-  warning: WARNING,
-  error: ERROR,
+  primary: { LIGHT_PRIMARY, DARK_PRIMARY },
+  secondary: { LIGHT_SECONDARY, DARK_SECONDARY },
+  error: { LIGHT_ERROR, DARK_ERROR },
+  warning: { LIGHT_WARNING, DARK_WARNING },
+  info: { LIGHT_INFO, DARK_INFO },
+  success: { LIGHT_SUCCESS, DARK_SUCCESS },
 };
 
 export default Colors;
--- src/theme/Palette.ts
+++ src/theme/Palette.ts
@@ -1,46 +1,81 @@
-import { alpha, PaletteOptions } from '@mui/material';
+import { PaletteOptions } from '@mui/material';
 import Colors from './Colors';
 
 const getPalette = (mode: 'light' | 'dark'): PaletteOptions => {
+  const pallet = {
+    light: {
+      primary: Colors.primary.LIGHT_PRIMARY,
+      secondary: Colors.secondary.LIGHT_SECONDARY,
+      error: Colors.error.LIGHT_ERROR,
+      warning: Colors.warning.LIGHT_WARNING,
+      info: Colors.info.LIGHT_INFO,
+      success: Colors.success.LIGHT_SUCCESS,
+      background: {
+        default: Colors.lightBackgroundColor,
+        paper: Colors.lightBackgroundColor,
+      },
+      text: {
+        primary: 'rgba(0, 0, 0, 0.87)',
+        secondary: 'rgba(0, 0, 0, 0.6)',
+        disabled: 'rgba(0, 0, 0, 0.38)',
+      },
+      divider: 'rgba(0, 0, 0, 0.12)',
+      action: {
+        active: 'rgba(0, 0, 0, 0.54)',
+        hover: 'rgba(0, 0, 0, 0.04)',
+        hoverOpacity: 0.04,
+        selected: 'rgba(0, 0, 0, 0.08)',
+        selectedOpacity: 0.08,
+        disabled: 'rgba(0, 0, 0, 0.26)',
+        disabledBackground: 'rgba(0, 0, 0, 0.12)',
+        disabledOpacity: 0.38,
+        focus: 'rgba(0, 0, 0, 0.12)',
+        focusOpacity: 0.12,
+        activatedOpacity: 0.12,
+      },
+    },
+    dark: {
+      primary: Colors.primary.DARK_PRIMARY,
+      secondary: Colors.secondary.DARK_SECONDARY,
+      error: Colors.error.DARK_ERROR,
+      warning: Colors.warning.DARK_WARNING,
+      info: Colors.info.DARK_INFO,
+      success: Colors.success.DARK_SUCCESS,
+      background: {
+        default: Colors.darkBackgroundColor,
+        paper: Colors.darkBackgroundColor,
+      },
+      text: {
+        primary: Colors.white,
+        secondary: 'rgba(255, 255, 255, 0.7)',
+        disabled: 'rgba(255, 255, 255, 0.5)',
+        icon: 'rgba(255, 255, 255, 0.5)',
+      },
+      divider: 'rgba(255, 255, 255, 0.12)',
+      action: {
+        active: Colors.white,
+        hover: 'rgba(255, 255, 255, 0.08)',
+        hoverOpacity: 0.08,
+        selected: 'rgba(255, 255, 255, 0.16)',
+        selectedOpacity: 0.16,
+        disabled: 'rgba(255, 255, 255, 0.3)',
+        disabledOpacity: 0.38,
+        disabledBackground: 'rgba(255, 255, 255, 0.12)',
+        focus: 'rgba(255, 255, 255, 0.12)',
+        focusOpacity: 0.12,
+        activatedOpacity: 0.24,
+      },
+    },
+  };
+
   return {
+    ...pallet[mode],
     mode: mode,
     common: {
       white: Colors.white,
       black: Colors.black,
     },
-    background: {
-      default:
-        mode === 'light'
-          ? Colors.lightBackgroundColor
-          : Colors.darkBackgroundColor,
-      paper:
-        mode === 'light'
-          ? Colors.lightBackgroundColor
-          : Colors.darkBackgroundColor,
-    },
-    primary: Colors.primary,
-    secondary: Colors.secondary,
-    info: Colors.info,
-    success: Colors.success,
-    error: Colors.error,
-    warning: Colors.warning,
     grey: Colors.grey,
-    divider: alpha(Colors.grey[500], 0.24),
-    text: {
-      primary: Colors.grey[800],
-      secondary: Colors.grey[600],
-      disabled: Colors.grey[500],
-    },
-    action: {
-      active: Colors.grey[600],
-      hover: alpha(Colors.grey[500], 0.08),
-      selected: alpha(Colors.grey[500], 0.16),
-      disabled: alpha(Colors.grey[500], 0.8),
-      disabledBackground: alpha(Colors.grey[500], 0.24),
-      focus: alpha(Colors.grey[500], 0.24),
-      hoverOpacity: 0.08,
-      disabledOpacity: 0.48,
-    },
   };
 };
 
