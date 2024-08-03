17ad0ae7939e383d8448a466984d2116d75530d7
Khaled Elorbany <khaled.wagdy554@gmail.com>
2024-08-03 23:58:36 +0300

    refactor: Remove unused imports and update Iconify component

--- src/shared/components/iconify/Iconify.tsx
+++ src/shared/components/iconify/Iconify.tsx
@@ -1,7 +1,6 @@
 import { forwardRef } from 'react';
 import { Icon } from '@iconify/react';
-import { Box, useTheme } from '@mui/material';
-import Colors from '@/theme/Colors';
+import { Box } from '@mui/material';
 
 type IconifyProps = {
   id?: string;
@@ -14,16 +13,13 @@ type IconifyProps = {
 
 const Iconify = forwardRef<HTMLDivElement, IconifyProps>(
   ({ icon, width = 20, color, sx, ...other }, ref) => {
-    const { palette } = useTheme();
     return (
       <Box
         id={other.id}
         ref={ref}
         component={Icon}
         icon={icon}
-        color={
-          palette.mode === 'dark' ? Colors.grey[400] : color || Colors.grey[800]
-        }
+        color={color}
         sx={{ width, height: width, ...sx }}
         onClick={other.onClick}
         {...other}
