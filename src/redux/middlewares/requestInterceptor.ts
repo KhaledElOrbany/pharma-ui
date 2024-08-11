ac7e0e7e903173c13d53b088795dc8e693f38b02
Khaled Elorbany <khaled.wagdy554@gmail.com>
2024-08-11 21:24:16 +0300

    clear localstorage if 401

--- src/redux/middlewares/requestInterceptor.ts
+++ src/redux/middlewares/requestInterceptor.ts
@@ -2,7 +2,7 @@ const RequestInterceptor = () => (next: any) => (action: any) => {
   if (action.payload?.status === 500) {
     window.location.href = '/500';
   } else if (action.payload?.status === 401 && localStorage.getItem('token')) {
-    localStorage.removeItem('token');
+    localStorage.clear();
 
     setTimeout(() => {
       if (typeof window !== 'undefined') window.location.reload();
