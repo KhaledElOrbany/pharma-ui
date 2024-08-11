79eca53317ce0ef96aa9ea3a9e9815fb299bcaea
Khaled Elorbany <khaled.wagdy554@gmail.com>
2024-08-11 21:25:38 +0300

    address module

---
+++ src/modules/address/redux/AddressAPI.ts
@@ -0,0 +1,58 @@
+import { createApi } from '@reduxjs/toolkit/query/react';
+import { customBaseQueryWithAuth } from '@/redux/baseQuery';
+import { errorHandler } from '@/helpers/components/ErrorHandler';
+import { generateUrlParams } from '@/helpers/utils/ParamsUtil';
+import { CityProps, GovernorateProps } from '../types/Address';
+
+export const AddressAPI = createApi({
+  reducerPath: 'addressAPI',
+  refetchOnReconnect: true,
+  tagTypes: ['cities', 'governorates'],
+  baseQuery: customBaseQueryWithAuth,
+  endpoints: (build) => ({
+    fetchCities: build.query({
+      query: (params) => ({
+        url: `/city/list?${
+          params.filters ? generateUrlParams([...params.filters]) : ''
+        }`,
+        method: 'GET',
+      }),
+      transformResponse: (result: { data: CityProps[] }) => {
+        return result.data;
+      },
+      async onQueryStarted(_, { dispatch, queryFulfilled }) {
+        queryFulfilled
+          .then(() => {
+            //
+          })
+          .catch(({ error }) => {
+            errorHandler(dispatch, error);
+          });
+      },
+      providesTags: ['cities'],
+    }),
+    fetchGovernorates: build.query<GovernorateProps[], void>({
+      query: () => ({
+        url: '/governorate/list',
+        method: 'GET',
+      }),
+      transformResponse: (result: { data: GovernorateProps[] }) => {
+        return result.data;
+      },
+      async onQueryStarted(_, { dispatch, queryFulfilled }) {
+        queryFulfilled
+          .then(() => {
+            //
+          })
+          .catch(({ error }) => {
+            errorHandler(dispatch, error);
+          });
+      },
+      providesTags: ['governorates'],
+    }),
+  }),
+});
+
+export const { useLazyFetchCitiesQuery, useLazyFetchGovernoratesQuery } =
+  AddressAPI;
+export default AddressAPI;
---
+++ src/modules/address/types/Address.d.ts
@@ -0,0 +1,13 @@
+type CityProps = {
+  id: number;
+  nameAr: string;
+  nameEn: string;
+};
+
+type GovernorateProps = {
+  id: number;
+  nameAr: string;
+  nameEn: string;
+};
+
+export { CityProps, GovernorateProps };
