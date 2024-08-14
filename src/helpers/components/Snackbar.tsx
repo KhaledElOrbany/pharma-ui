dc9880460a4fdc0d6589b7d1a2bbe7621766c232
Khaled Elorbany <khaled.wagdy554@gmail.com>
2024-08-14 20:31:34 +0300

    updated Snackbar

--- src/helpers/components/Snackbar.tsx
+++ src/helpers/components/Snackbar.tsx
@@ -1,13 +1,13 @@
 import { toggleSnackbar } from '@/modules/home/redux/AppSlice';
 
-export const errorHandler = (dispatch: any, response: string) => {
+export const Snackbar = (dispatch: any, response: string, severity: string) => {
   const message = response ?? 'An error has occurred!';
 
   return dispatch(
     toggleSnackbar({
       open: true,
       message: message,
-      severity: 'error',
+      severity,
     })
   );
 };
